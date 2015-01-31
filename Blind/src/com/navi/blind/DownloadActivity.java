ff1c8d3edc038e18aabe7aa2eaeffcfdf8d76f1a
cantbesure <surelee0@gmail.com>
2015-01-31 12:24:38 +0800

    refine

--- Blind/src/com/navi/blind/DownloadActivity.java
+++ Blind/src/com/navi/blind/DownloadActivity.java
@@ -7,12 +7,17 @@ import com.navi.client.Conmmunication;
 import com.navi.client.Constant;
 import com.navi.model.Path;
 import com.navi.util.Database;
+import com.navi.util.PathOperationService;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
+import android.content.ComponentName;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -23,6 +28,32 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class DownloadActivity extends BaseActivity {
+	private PathOperationService.MyBinder path_binder;
+	private Intent intent_path_service ;
+	private ServiceConnection connection_path = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder service) {
+			path_binder = (PathOperationService.MyBinder) service;
+			path_binder.downloadInitService();
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			
+		}
+	};
+	
+	@Override
+	protected void onStart() {
+		
+		super.onStart();
+	}
+
+	@Override
+	protected void onDestroy() {
+		stopService(intent_path_service);
+		super.onDestroy();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +109,10 @@ public class DownloadActivity extends BaseActivity {
 			List<Path> list = (List<Path>) message.obj;
 			Database db = Database.getInstance(this);
 			db.writePaths(list);
+			intent_path_service = new Intent(this,
+					PathOperationService.class);
+			startService(intent_path_service);
+			bindService(intent_path_service, connection_path, BIND_AUTO_CREATE);
 			break;
 		case Config.REQUEST_PATHINFO:
 			Path path = (Path) message.obj;
